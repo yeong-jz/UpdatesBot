aa7eee64d1d97e3e92b0804642e1730d12cbb4a8
yeong jinzhi <yeong_jz@hotmail.com>
2019-05-24 02:03:56 +0800

    minor update

--- updatesbot.py
+++ updatesbot.py
@@ -1,3 +1,7 @@
 import feedparser
+from bs4 import BeautifulSoup as bs
+
 
 feed = feedparser.parse("https://rss.app/feeds/X16Cm8dsU7VBEHVp.xml")
+summary = feed['entries'][0]['summary']
+cleantext = bs(summary, "lxml").text
